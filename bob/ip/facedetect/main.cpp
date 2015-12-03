3c2e10ade00e03b55de67c4ef1be86c191b71db7
Manuel Gunther <siebenkopf@googlemail.com>
2015-12-03 13:51:33 -0700

    Corrected error cases in module creation

--- bob/ip/facedetect/main.cpp
+++ bob/ip/facedetect/main.cpp
@@ -158,11 +158,13 @@ static PyObject* create_module (void) {
 
 # if PY_VERSION_HEX >= 0x03000000
   PyObject* module = PyModule_Create(&module_definition);
+  auto module_ = make_xsafe(module);
+  const char* ret = "O";
 # else
   PyObject* module = Py_InitModule3(BOB_EXT_MODULE_NAME, module_methods, module_docstr);
+  const char* ret = "N";
 # endif
   if (!module) return 0;
-  auto module_ = make_safe(module); ///< protects against early returns
 
   if (!init_BobIpFacedetectBoundingBox(module)) return 0;
   if (!init_BobIpFacedetectFeatureExtractor(module)) return 0;
@@ -173,7 +175,7 @@ static PyObject* create_module (void) {
   if (import_bob_io_base() < 0) return 0;
   if (import_bob_ip_base() < 0) return 0;
 
-  return Py_BuildValue("O", module);
+  return Py_BuildValue(ret, module);
 }
 
 PyMODINIT_FUNC BOB_EXT_ENTRY_NAME (void) {
