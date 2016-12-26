0ef5e467c8b95f3ff7467f506ebcf6362418280d
Amir Mohammadi <183.amir@gmail.com>
2016-12-26 14:08:12 +0100

    [doc] change the title of the guide

--- doc/plot/detect_single_face.py
+++ doc/plot/detect_single_face.py
@@ -5,14 +5,11 @@ import bob.ip.facedetect
 import bob.ip.draw
 
 # load colored test image
-import pkg_resources
 color_image = bob.io.base.load(bob.io.base.test_utils.datafile('testimage.jpg', 'bob.ip.facedetect'))
 
 # detect single face
 bounding_box, _ = bob.ip.facedetect.detect_single_face(color_image)
 
 # create figure
-from matplotlib import pyplot
-import numpy
-bob.ip.draw.box(color_image, bounding_box.topleft, bounding_box.size, color=(255,0,0))
-pyplot.imshow(numpy.rollaxis(numpy.rollaxis(color_image, 2),2))
+bob.ip.draw.box(color_image, bounding_box.topleft, bounding_box.size, color=(255, 0, 0))
+bob.io.image.imshow(color_image)
