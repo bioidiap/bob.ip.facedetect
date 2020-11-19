c8c88696928e680d040edf05291f9f19e69bcef7
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2020-11-19 17:35:13 +0100

    [doc] only run mtcnn example when tf is available

--- doc/plot/detect_faces_mtcnn.py
+++ doc/plot/detect_faces_mtcnn.py
@@ -1,53 +1,63 @@
-from bob.io.image import imshow
+import matplotlib.pyplot as plt
 from bob.io.base import load
 from bob.io.base.test_utils import datafile
+from bob.io.image import imshow
 from bob.ip.facedetect.mtcnn import MTCNN
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle, Circle
+from bob.ip.facedetect.tests.utils import is_library_available
+from matplotlib.patches import Circle
+from matplotlib.patches import Rectangle
 
 # load colored test image
 color_image = load(datafile("test_image_multi_face.png", "bob.ip.facedetect"))
 
-# detect all face
-detector = MTCNN()
-detections = detector(color_image)
+if not is_library_available("tensorflow"):
+    imshow(color_image)
+else:
 
-imshow(color_image)
-plt.axis("off")
+    # detect all face
+    detector = MTCNN()
+    detections = detector(color_image)
 
-for annotations in detections:
-    topleft = annotations["topleft"]
-    bottomright = annotations["bottomright"]
-    size = bottomright[0] - topleft[0], bottomright[1] - topleft[1]
-    # draw bounding boxes
-    plt.gca().add_patch(
-        Rectangle(
-            topleft[::-1],
-            size[1],
-            size[0],
-            edgecolor="b",
-            facecolor="none",
-            linewidth=2,
-        )
-    )
-    # draw face landmarks
-    for key, color in (
-        ("reye", "r"),
-        ("leye", "g"),
-        ("nose", "b"),
-        ("mouthright", "k"),
-        ("mouthleft", "w"),
-    ):
+    imshow(color_image)
+    plt.axis("off")
+
+    for annotations in detections:
+        topleft = annotations["topleft"]
+        bottomright = annotations["bottomright"]
+        size = bottomright[0] - topleft[0], bottomright[1] - topleft[1]
+        # draw bounding boxes
         plt.gca().add_patch(
-            Circle(
-                annotations[key][::-1],
-                radius=2,
-                edgecolor=color,
+            Rectangle(
+                topleft[::-1],
+                size[1],
+                size[0],
+                edgecolor="b",
                 facecolor="none",
                 linewidth=2,
             )
         )
-    # show quality of detections
-    plt.text(
-        topleft[1], topleft[0], round(annotations["quality"], 3), color="b", fontsize=14
-    )
+        # draw face landmarks
+        for key, color in (
+            ("reye", "r"),
+            ("leye", "g"),
+            ("nose", "b"),
+            ("mouthright", "k"),
+            ("mouthleft", "w"),
+        ):
+            plt.gca().add_patch(
+                Circle(
+                    annotations[key][::-1],
+                    radius=2,
+                    edgecolor=color,
+                    facecolor="none",
+                    linewidth=2,
+                )
+            )
+        # show quality of detections
+        plt.text(
+            topleft[1],
+            topleft[0],
+            round(annotations["quality"], 3),
+            color="b",
+            fontsize=14,
+        )
